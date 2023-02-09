71a3dc19ee169e21ec3a54f05bb99807165d94f9
Galen <gnare@udel.edu>
2023-02-09 14:45:15 -0500

    Add cool gif

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,10 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                <img
+                    src="https://media4.giphy.com/media/9V8YiWAcXYxT14YXHI/giphy.gif"
+                    alt="Bongo cat"
+                />
             </p>
         </div>
     );

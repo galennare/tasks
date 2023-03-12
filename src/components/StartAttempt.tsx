db55825eb126059831ec3a7cbf9d1451fbf71c41
Galen <gnare@udel.edu>
2023-03-12 00:38:33 -0500

    Task 8 reveal

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,7 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
+    const [inProgress, setInProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
     return <div>Start Attempt</div>;
 }

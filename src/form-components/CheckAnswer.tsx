d02389ad3db12e621dfca7855dff8af9bf68c403
Galen Nare <gnare@udel.edu>
2023-03-14 15:19:49 -0400

    Task 10 partial

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
     return (
         <div>
-            <h3>Check Answer</h3>
+            <span style={{ fontSize: "18pt", fontWeight: "bold" }}>
+                Check Answer
+            </span>
+            <Form.Control
+                as="textarea"
+                rows={1}
+                value={answer}
+                onChange={(e: {
+                    target: { value: React.SetStateAction<string> };
+                }) => setAnswer(e.target.value)}
+            />
+            {answer == expectedAnswer ? "\u2714\uFE0F" : "\u274C"}
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 
 export function EditMode(): JSX.Element {
+    const [inEditMode, setInEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("");
+    const [isStudent, setIsStudent] = useState<boolean>(false);
+
     return (
         <div>
             <h3>Edit Mode</h3>

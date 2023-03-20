734d86e6ee9d964b3ceb65efb2398e164ec008c8
Galen Nare <gnare@udel.edu>
2023-03-20 12:09:05 -0400

    Task 10 EditMode test wants you to be a mind reader apparently

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,13 +1,47 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
     const [inEditMode, setInEditMode] = useState<boolean>(false);
-    const [name, setName] = useState<string>("");
-    const [isStudent, setIsStudent] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
 
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <span style={{ fontSize: "18pt", fontWeight: "bold" }}>
+                Edit Mode
+            </span>
+
+            <Form.Switch
+                type="switch"
+                checked={inEditMode}
+                label="Enable Edit Mode"
+                onChange={(e) => setInEditMode(e.target.checked)}
+            />
+
+            {inEditMode ? (
+                <div>
+                    <Form.Control
+                        type="text"
+                        value={name}
+                        onChange={(e: {
+                            target: { value: React.SetStateAction<string> };
+                        }) => setName(e.target.value)}
+                    />
+                    <Form.Check
+                        type="checkbox"
+                        id="student"
+                        checked={isStudent}
+                        label="Student?"
+                        onChange={(e) => setIsStudent(e.target.checked)}
+                    />
+                </div>
+            ) : (
+                <div></div>
+            )}
+
+            {name}
+            {isStudent ? " is a student." : " is not a student."}
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Button, Form} from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
     const [attempts, setAttempts] = useState<number>(3);

211ffb84355575d019c1611e3f55578254f48e8a
Galen Nare <gnare@udel.edu>
2023-03-20 12:20:46 -0400

    Task 10 change color is left

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <span style={{ fontSize: "18pt", fontWeight: "bold" }}>
+                Multiple Choice Question
+            </span>
+
+            <Form.Group controlId="userEmotions">
+                <Form.Label>How do you feel?</Form.Label>
+                <Form.Select
+                    value={options[0]}
+                    onChange={(e) => setAnswer(e.target.value)}
+                >
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {answer == expectedAnswer ? "\u2714\uFE0F" : "\u274C"}
         </div>
     );
 }
